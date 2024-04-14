d40ccca5df8f2af2a3c244f69aec756db3a65326
Pieterbas Nagengast <pieterbas.nagengast@microsoft.com>
2024-04-14 23:30:41 +0200

    add validation

--- Copy-AzFwPolicy.ps1
+++ Copy-AzFwPolicy.ps1
@@ -1,19 +1,53 @@
-# Description: Copy an Azure Firewall Policy from one subscription to another subscription
+<#
+.SYNOPSIS
+Copy an Azure Firewall Policy from one subscription to another subscription
 
-# enable command line binding
+.DESCRIPTION
+Copy an Azure Firewall Policy from one subscription to another subscription
+
+.PARAMETER SourceAzFwPolicyID
+The Source Azure Firewall Policy ResourceID
+
+.PARAMETER TargetAzFwPolicyID
+The Target Azure Firewall Policy ResourceID
+
+.EXAMPLE
+Copy-AzFwPolicy.ps1 -SourceAzFwPolicyID "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/rg-source/providers/Microsoft.Network/FirewallPolicies/azfw-source" -TargetAzFwPolicyID "/subscriptions/11111111-1111-1111-1111-111111111111/resourceGroups/rg-target/providers/Microsoft.Network/FirewallPolicies/azfw-target" -IncludeTags
+
+#>
+
 [CmdletBinding()]
-# commandline parameters make them mandatory, including includeTags toggle
+# parameters make, mandatory, including includeTags switch
 param (
     [Parameter(Mandatory = $true)]
+    [Validatescript({
+            if (-not ($_ -like "*/providers/Microsoft.Network/FirewallPolicies/*")) {
+                throw "The SourceAzFwPolicyID parameter must be a valid Azure Firewall Policy ID."
+            }
+            return $true
+        }
+    )]
     [string]$SourceAzFwPolicyID,
     [Parameter(Mandatory = $true)]
-    [string]$TargetAzFwPolicyID
+    [Validatescript({
+            if (-not ($_ -Like "*/providers/Microsoft.Network/FirewallPolicies/*")) {
+                throw "The TargetAzFwPolicyID parameter must be a valid Azure Firewall Policy ID."
+            }
+            return $true
+        }
+    )]
+    [string]$TargetAzFwPolicyID,
+    [switch]$IncludeTags
 )
-    
+
+# validate the source and target firewall policy IDs are different
+if ($TargetAzFwPolicyID -eq $SourceAzFwPolicyID) {
+    throw "The SourceAzFwPolicyID and TargetAzFwPolicyID parameters must be different."
+}
 
 # get the source and target firewall policies
-$SourceAzFwPolicy = Get-AzFirewallPolicy -ResourceId $SourceAzFwPolicyID
-$TargetAzFwPolicy = Get-AzFirewallPolicy -ResourceId $TargetAzFwPolicyID
+$SourceAzFwPolicy = Get-AzFirewallPolicy -ResourceId $SourceAzFwPolicyID -ErrorAction Stop
+$TargetAzFwPolicy = Get-AzFirewallPolicy -ResourceId $TargetAzFwPolicyID -ErrorAction Stop
 
 # input object for set-azfirewallpolicy
 $TargetAzFwPolicy = @{
@@ -29,6 +63,7 @@ $TargetAzFwPolicy = @{
     Sqlsetting           = $SourceAzFwPolicy.sqlsetting
     BasePolicy           = $SourceAzFwPolicy.BasePolicy
     Snat                 = $SourceAzFwPolicy.Snat
+    Tag                  = if ($IncludeTags) { $SourceAzFwPolicy.Tag } else { $null }
 }
 
 # for each rule collection group in the source firewall policy
