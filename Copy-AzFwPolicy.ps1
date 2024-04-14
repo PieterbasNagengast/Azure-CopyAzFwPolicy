87c616a01d999226c1e127b56436c85fc43b0f63
Pieterbas Nagengast <pieterbas.nagengast@microsoft.com>
2024-04-14 18:34:04 +0200

    initial

---
+++ Copy-AzFwPolicy.ps1
@@ -0,0 +1,45 @@
+# Description: Copy an Azure Firewall Policy from one subscription to another subscription
+
+# enable command line binding
+[CmdletBinding()]
+# commandline parameters make them mandatory
+param (
+    [Parameter(Mandatory = $true)]
+    [string]$SourceAzFwPolicyID,
+    [Parameter(Mandatory = $true)]
+    [string]$TargetAzFwPolicyID
+)
+
+# get the source and target firewall policies
+$SourceAzFwPolicy = Get-AzFirewallPolicy -ResourceId $SourceAzFwPolicyID
+$TargetAzFwPolicy = Get-AzFirewallPolicy -ResourceId $TargetAzFwPolicyID
+
+# input object for set-azfirewallpolicy
+$TargetAzFwPolicy = @{
+    ResourceGroupName    = $TargetAzFwPolicy.ResourceGroupName
+    Name                 = $TargetAzFwPolicy.Name
+    Location             = $TargetAzFwPolicy.Location
+    DnsSettings          = $SourceAzFwPolicy.DnsSettings
+    ThreatIntelMode      = $SourceAzFwPolicy.ThreatIntelMode
+    ThreatIntelWhitelist = $SourceAzFwPolicy.ThreatIntelWhitelist
+    ExplicitProxy        = $SourceAzFwPolicy.explicitProxy
+    IntrusionDetection   = $SourceAzFwPolicy.IntrusionDetection
+    transportSecurity    = $SourceAzFwPolicy.transportSecurity
+    Sqlsetting           = $SourceAzFwPolicy.sqlsetting
+    BasePolicy           = $SourceAzFwPolicy.BasePolicy
+    Snat                 = $SourceAzFwPolicy.Snat
+    Tag                  = $SourceAzFwPolicy.Tag
+}
+
+# for each rule collection group in the source firewall policy
+foreach ($SourceAzFwPolicyRuleCollectionGroup in $SourceAzFwPolicy.RuleCollectionGroups) {
+    # get the source firewall policy rule collection group
+    $RuleCollectionGroup = Get-AzFirewallPolicyRuleCollectionGroup -AzureFirewallPolicyName $SourceAzFwPolicy.Name -ResourceGroupName $SourceAzFwPolicy.ResourceGroupName -Name ($SourceAzFwPolicyRuleCollectionGroup.id).Split("/")[-1]
+
+    # set the target firewall policy rule collection group
+    New-AzFirewallPolicyRuleCollectionGroup -Name $RuleCollectionGroup.Name -ResourceGroupName $TargetAzFwPolicy.ResourceGroupName -FirewallPolicyName $TargetAzFwPolicy.Name -Priority $RuleCollectionGroup.Properties.Priority -RuleCollection $RuleCollectionGroup.Properties.RuleCollection
+    
+}
+
+# set the target firewall policy
+Set-AzFirewallPolicy -InputObject $TargetAzFwPolicy
