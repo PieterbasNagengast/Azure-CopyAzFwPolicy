ba7087cdb460e501a26cebc08b0dabc9f54fa009
Pieterbas Nagengast <pieterbas.nagengast@microsoft.com>
2024-04-15 23:41:21 +0200

    updates

--- Copy-AzFwPolicy.ps1
+++ Copy-AzFwPolicy.ps1
@@ -43,13 +43,29 @@ param (
 # set default error action preference
 $ErrorActionPreference = "Stop"
 
+$sourceSubscriptionId = $SourceAzFwPolicyID.Split("/")[2]
+$targetSubscriptionId = $TargetAzFwPolicyID.Split("/")[2]
+
 # validate the source and target firewall policy IDs are different
 if ($TargetAzFwPolicyID -eq $SourceAzFwPolicyID) {
     Write-Error "The SourceAzFwPolicyID and TargetAzFwPolicyID parameters must be different."
 }
 
-if (-not ($TargetAzFwPolicyID.Split("/")[2] -eq $SourceAzFwPolicyID.Split("/")[2])) {
-    Write-Error "The SourceAzFwPolicyID and TargetAzFwPolicyID parameters must be in the same subscription."
+# validate if we need to swtich context to source firewall policy subscription
+Write-Host "Checking current context..."
+$CurrentAzContext = Get-AzContext
+
+if (!$CurrentAzContext) {
+    Write-Error "Failed to get current Azure context."
+}
+
+if ($CurrentAzContext.Subscription.Id -ne $sourceSubscriptionId) {
+    $sourceSubscription = Get-AzSubscription -SubscriptionId $sourceSubscriptionId
+    write-host "Switching to source subscription: $($sourceSubscription.Name)"
+    $CurrentAzContext = Set-AzContext -Subscription $sourceSubscription
+}
+else {
+    Write-Host "Current context is already in the correct subscription: $($CurrentAzContext.Subscription.Name)" -ForegroundColor Green
 }
 
 # get the source and target firewall policies
@@ -58,6 +74,15 @@ $SourceAzFwPolicy = Get-AzFirewallPolicy -ResourceId $SourceAzFwPolicyID -ErrorA
 
 if ($null -eq $SourceAzFwPolicy) {
     Write-Error "Failed to get Azure Firewall Source Policy: $($SourceAzFwPolicyID)."
+}
+
+if ($CurrentAzContext.Subscription.Id -ne $targetSubscriptionId) {
+    $targetSubscription = Get-AzSubscription -SubscriptionId $targetSubscriptionId
+    write-host "Switching to target subscription: $($targetSubscription.Name)"
+    $CurrentAzContext = Set-AzContext -Subscription $targetSubscription
+}
+else {
+    Write-Host "Current context is already in the correct subscription: $($CurrentAzContext.Subscription.Name)" -ForegroundColor Green
 }
 
 Write-Verbose "Getting Azure Firewall Policy: $($TargetAzFwPolicyID)"
@@ -97,6 +122,17 @@ $TargetAzFwPolicy = @{
 
 Write-Verbose ($TargetAzFwPolicy | ConvertTo-Json -Depth 100)
 
+Write-Host "Copying Azure Firewall Policy $($SourceAzFwPolicy.Name) to $($TargetAzFwPolicy.Name)..."
+# set the target firewall policy
+$setPolicy = Set-AzFirewallPolicy -InputObject $TargetAzFwPolicy
+
+if ($setPolicy) {
+    Write-Host "Azure Firewall Policy $($SourceAzFwPolicy.Name) copied successfully." -ForegroundColor Green
+}
+else {
+    Write-Error "Failed to copy Azure Firewall Policy $($SourceAzFwPolicy.Name)."
+}
+
 Write-Host "Getting Rule Collection Groups..."
 # for each rule collection group in the source firewall policy
 foreach ($SourceAzFwPolicyRuleCollectionGroup in $SourceAzFwPolicy.RuleCollectionGroups) {
@@ -104,6 +140,7 @@ foreach ($SourceAzFwPolicyRuleCollectionGroup in $SourceAzFwPolicy.RuleCollectio
     $RuleCollectionGroup = Get-AzFirewallPolicyRuleCollectionGroup -AzureFirewallPolicyName $SourceAzFwPolicy.Name -ResourceGroupName $SourceAzFwPolicy.ResourceGroupName -Name ($SourceAzFwPolicyRuleCollectionGroup.id).Split("/")[-1]
 
     Write-Host "Copying Rule Collection Group: $($RuleCollectionGroup.Name)..."
+    write-verbose "Copying Rule Collection(s): $($RuleCollectionGroup.Properties.RuleCollection.name) with a total of $(($RuleCollectionGroup.Properties.RuleCollection.rules).Count) rule(s)"
     # set the target firewall policy rule collection group
     $newRuleCollectionGroup = New-AzFirewallPolicyRuleCollectionGroup -Name $RuleCollectionGroup.Name -ResourceGroupName $TargetAzFwPolicy.ResourceGroupName -FirewallPolicyName $TargetAzFwPolicy.Name -Priority $RuleCollectionGroup.Properties.Priority -RuleCollection $RuleCollectionGroup.Properties.RuleCollection
     
@@ -116,13 +153,3 @@ foreach ($SourceAzFwPolicyRuleCollectionGroup in $SourceAzFwPolicy.RuleCollectio
 
 }
 
-Write-Host "Copying Azure Firewall Policy $($SourceAzFwPolicy.Name) to $($TargetAzFwPolicy.Name)..."
-# set the target firewall policy
-$setPolicy = Set-AzFirewallPolicy -InputObject $TargetAzFwPolicy
-
-if ($setPolicy) {
-    Write-Host "Azure Firewall Policy $($SourceAzFwPolicy.Name) copied successfully." -ForegroundColor Green
-}
-else {
-    Write-Error "Failed to copy Azure Firewall Policy $($SourceAzFwPolicy.Name)."
-}
